7c031a64ef851afedbdbdf32bdd14d5433155d84
nawhes <nawhes@naver.com>
2022-03-30 23:45:50 +0900

    feat: MemoService 개발

--- src/errors/DataAccessError.ts
+++ src/errors/DataAccessError.ts
@@ -3,6 +3,6 @@ import { BaseError } from './BaseError';
 export default class DataAccessError extends BaseError {
 	constructor(message: string) {
 		super(message);
-		this.status = 500;
+		this.status = 401;
 	}
 }
